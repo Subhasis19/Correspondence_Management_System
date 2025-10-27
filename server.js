440324df20bbf6f82ff89eff0ff8e9be4926f91c
Subhasis Samantasinghar <117301784+Subhasis19@users.noreply.github.com>
2025-10-27 16:16:44 +0530

    Add frontend files for login and registration forms, update server static directory

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require("./db");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static("frontend"));
 app.use(
   session({
     secret: "secretkey",
